6ecbe9c2f42ee749a8ccb8b4914b361be68d7a6c
MineRaphi <MineRaphiHD@gmail.com>
2026-09-23 09:12:43 +0200

    updated api calls to CapacitorHttp, still needs to updated respond handling

--- scripts/api.js
+++ scripts/api.js
@@ -1,66 +1,85 @@
 import { CapacitorHttp } from '@capacitor/core';
 
-const URL = `https://notenmanagement.htl-braunau.at/rest`;
-//const URL = `http://127.0.0.1:8000`;
+//const URL = `https://notenmanagement.htl-braunau.at/rest`;    // real life url
+const URL = `http://127.0.0.1:8000`;    // dev testing url
 
 export async function loginRequest(username, password) {
-    return fetch(URL + '/Token', {
+    return CapacitorHttp.request({
         method: 'POST',
+        url: URL + '/Token',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: `grant_type=password&username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
-    });
+    })
 }
 
 export async function getStudentInfo(matrikelNr, token) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getLatestGrades(matrikelNr, token) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}/Noten?limit=5&sort=-Datum`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}/Noten?limit=5&sort=-Datum`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getSubjectsWithGrade(matrikelNr, token) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}/Faecher?sort=Fach`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}/Faecher?sort=Fach`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getGradesFromSubject(matrikelNr, token, subject) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}/Faecher/${subject}/Noten?sort=-Datum`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}/Faecher/${subject}/Noten?sort=-Datum`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getFruewarnungen(matrikelNr, token) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}/Fruehwarnungen?sort=Fach`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}/Fruehwarnungen?sort=Fach`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getFehlstunden(matrikelNr, token) {
-    return fetch(URL + `/api/Schueler/${matrikelNr}/Fehlstunden`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Schueler/${matrikelNr}/Fehlstunden`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getLFdata(_matrikelNr, token, LF_ID) {
-    return fetch(URL + `/api/LFs/${LF_ID}`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/LFs/${LF_ID}`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
 export async function getLFgrade(matrikelNr, token, LF_ID) {
-    return fetch(URL + `/api/LFs/${LF_ID}/Schueler/${matrikelNr}/Noten`, {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/LFs/${LF_ID}/Schueler/${matrikelNr}/Noten`,
         headers: { 'Authorization': `bearer ${token}` }
-    });
+    })
 }
 
-export async function getLehrer(matrikelNr, token) {
-    return fetch(URL + `/api/Lehrer`, {
+export async function getLehrer(_matrikelNr, token) {
+    return CapacitorHttp.request({
+        method: 'GET',
+        url: URL + `/api/Lehrer`,
         headers: { 'Authorization': `bearer ${token}` }
     });
 }
@@ -78,7 +97,7 @@ export async function getLehrerListUntis() {
 }
 
 export async function getLehrerDataUntis(teacherID) {
-    return await CapacitorHttp.request({
+    return CapacitorHttp.request({
         method: 'POST',
         url: 'https://services01.htl-braunau.at/WhereIsMyTeacher/data.php',
         headers: {
