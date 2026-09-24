63e7054d3471573d0fbcd60e80a61b673da80e46
MineRaphi <MineRaphiHD@gmail.com>
2026-09-24 11:40:14 +0200

    exported API_URL

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
-//const API_URL = `https://notenmanagement.htl-braunau.at/rest`;    // real life url
-const API_URL = `http://127.0.0.1:8000`;    // dev testing url
+//export const API_URL = `https://notenmanagement.htl-braunau.at/rest`;    // real life url
+export const API_URL = `http://127.0.0.1:8000`;    // dev testing url
 
 export const TIMEOUT_MESSAGE = "Connection failed. Check your internet.";
 export const DEFAULT_TIMEOUT_MS = 5000
